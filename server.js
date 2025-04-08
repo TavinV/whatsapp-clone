5ad81ea63d3ab7f356f1778c5a2e6c2a4d97b46d
TavinV <otavioviniciusads@gmai.com>
2025-04-08 10:10:53 -0300

    update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const __dirname = path.dirname(__filename);
 
 // Configuração do Express
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 // Servir arquivos estáticos da pasta public
 app.use(express.static(path.join(__dirname, 'public')));
